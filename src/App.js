6e0770744ec77ba308abbc6ba8440528a26953e5
myosotisrose <natalia.pokusa@gmail.com>
2023-03-25 19:53:27 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a href="#" className="btn btn-primary">Hello</a>
+        <a href="/" className="btn btn-primary">Hello</a>
       </header>
     </div>
   );
